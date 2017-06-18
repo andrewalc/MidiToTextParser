be36dc6a323bfbe44b6297391a0f6da531b858f4
andrewalc <andrewalc98@gmail.com>
2017-06-18 18:50:18 -0400

    More JavaDoc.

--- src/MidiParser/model/RunMidiParser.java
+++ src/MidiParser/model/RunMidiParser.java
@@ -10,10 +10,10 @@ import javax.sound.midi.MidiUnavailableException;
  * Main method for running a MidiParser.
  */
 public class RunMidiParser {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     if (args.length != 1) {
       throw new IllegalArgumentException("Must give the midi file name:\n"
-          + "(ex.: \"smoke.mid\")\n");
+              + "(ex.: \"smoke.mid\")\n");
     }
     String midiFile = args[0];
     try {
@@ -21,11 +21,11 @@ public class RunMidiParser {
       parser.writeMidiTextFile();
       System.exit(0);
     } catch (InvalidMidiDataException e) {
-      e.printStackTrace();
+      System.out.println(e.getMessage());
     } catch (IOException e) {
-      e.printStackTrace();
+      System.out.println(e.getMessage());
     } catch (MidiUnavailableException e) {
-      e.printStackTrace();
+      System.out.println(e.getMessage());
     }
   }
 }
