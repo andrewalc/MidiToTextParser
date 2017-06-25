eb2c4f5cd266ded10389a301fe4f26fa51d71d57
andrewalc <andrewalc98@gmail.com>
2017-06-25 03:12:57 -0400

    JAR UI update. Now writes txt file to the directory the midi file was retrieved from.

--- src/MidiParser/model/RunMidiParser.java
+++ src/MidiParser/model/RunMidiParser.java
@@ -1,12 +1,5 @@
 package MidiParser.model;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiUnavailableException;
-
-import MidiParser.view.IMidiParserView;
 import MidiParser.view.MidiParserView;
 
 
@@ -15,26 +8,7 @@ import MidiParser.view.MidiParserView;
  */
 public class RunMidiParser {
   public static void main(String[] args) {
-//    if (args.length != 1) {
-//      throw new IllegalArgumentException("Must give the midi file name:\n"
-//              + "(ex.: \"smoke.mid\")\n");
-//    }
-//    String midiFile = args[0];
-
-
-    IMidiParserView view = new MidiParserView();
+    MidiParserView view = new MidiParserView();
     view.initialize();
-    File midiFile = view.getSelectedFile();
-    try {
-      MidiParser parser = new MidiParser(midiFile);
-      parser.writeMidiTextFile();
-      System.exit(0);
-    } catch (InvalidMidiDataException e) {
-      System.out.println(e.getMessage());
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    } catch (MidiUnavailableException e) {
-      System.out.println(e.getMessage());
-    }
   }
 }
